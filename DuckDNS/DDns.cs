181549027d71d8e39585b26e72e5e2cdc78af4b8
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2020-05-16 17:11:18 -0300

    Added support for IPv6.

--- DuckDNS/DDns.cs
+++ DuckDNS/DDns.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Net.Sockets;
 
 namespace DuckDNS
 {
@@ -15,10 +17,70 @@ namespace DuckDNS
         public string Interval;
         private WebClient cli = new WebClient();
 
-        public bool Update(){
-            string url="https://www.duckdns.org/update?domains="+Domain+"&token="+Token+"&ip=";
-            string data = cli.DownloadString(url);
-            return data == "OK";
+        /// <summary>
+        /// This method updates the Duck DNS Subdomain IP address.
+        /// </summary>
+        /// <returns>Boolean value whether it suceeds to update the subdomain IP address</returns>
+        public bool Update()
+        {
+            //Updating IPv4
+            string urlIPv4 = "https://www.duckdns.org/update?domains=" + Domain + "&token=" + Token + "&ip=";
+            string dataIPv4 = cli.DownloadString(urlIPv4);
+            
+
+            string dataIPv6 = "";
+
+            //Getting IPv6 addresses
+            ArrayList IPv6 = getIPv6();
+
+            if (IPv6.Capacity > 0)
+            {
+                String urlIPv6 = "https://www.duckdns.org/update?domains=" + Domain + "&token=" + Token + "&ipv6=" + IPv6[0].ToString();
+                dataIPv6 = cli.DownloadString(urlIPv6);
+            }
+
+            return (dataIPv4 == "OK" || dataIPv6 == "OK");
+        }
+
+        /// <summary>
+        /// This method gets the current public IPv6 addresses of the machine on which it is running.
+        /// </summary>
+        /// <returns>An ArrayList containing public IPv6 addresses.</returns>
+        private ArrayList getIPv6()
+        {
+            //Creating the ArrayList.
+            ArrayList IPv6Addresses = new ArrayList();
+
+            //Verifying if the Operating System supports IPv6.
+            if (Socket.OSSupportsIPv6)
+            {
+                try
+                {
+                    //Getting IP addresses.
+                    IPHostEntry heserver = Dns.GetHostEntry(Dns.GetHostName());
+
+                    //Finding public IPv6 addresses.
+                    foreach (IPAddress curAdd in heserver.AddressList)
+                    {
+                        if (curAdd.AddressFamily.ToString() == ProtocolFamily.InterNetworkV6.ToString() && (!curAdd.IsIPv6Multicast) && (!curAdd.IsIPv6LinkLocal) && (!curAdd.IsIPv6SiteLocal))
+                        {
+                            //Adding the adress to the ArrayList.
+                            IPv6Addresses.Add(curAdd);
+                        }
+                    }
+                }
+                catch (NotSupportedException e)
+                {
+                    Console.WriteLine(e);
+                }
+
+                catch (SocketException e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+
+            return IPv6Addresses;
         }
 
         public void Load()
